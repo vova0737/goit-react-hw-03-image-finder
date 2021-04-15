529b9a0763e9977e5dbcb7dfd1d477a413e93416
Volodymyr Bondar <vova0737@gmail.com>
2021-04-16 01:15:19 +0300

    update service

--- src/Services/Pixabay-api.js
+++ src/Services/Pixabay-api.js
@@ -3,12 +3,23 @@ import axios from 'axios';
 const KEY = '20353486-11ecb503bec0da706377a3524';
 const BASE_URL = 'https://pixabay.com/api/';
 
-const getImagesData = (query, page = 1) => {
-  const response = axios.get(
-    `${BASE_URL}?q=${query}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=10`,
-  );
-  return response;
+axios.defaults.baseURL = BASE_URL;
+axios.defaults.params = {
+  key: KEY,
+  image_type: 'photo',
+  orientation: 'horizontal',
+  per_page: 12,
+}
+
+const getImagesData = async ({ query, page }) => {
+  try {
+    const data = await axios.get('', { params: { query, page }, });
+    return data;
+  }
+  catch (error) {
+    console.log('Error', { error });
+    return [];
+  }
 };
 
 export default getImagesData;
-
